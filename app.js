8b82a9815885690b1e2a3d15d60a3e1e8fe676c9
cristian-morris <christianyahirnavarrochan@gmail.com>
2023-02-12 00:50:31 -0600

    requiriendo y estableciendo rutas
    requiriendo y estableciendo rutas

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+
+const dotenv = require('dotenv');
+dotenv.config({ path: './env/.env'});
+
+app.use('/resources',express.static('public'));
+app.use('/resources', express.static(__dirname + '/public'));
+
+
+app.set('view engine','ejs');
+
+
+const bcrypt = require('bcryptjs');
+
+
+const session = require('express-session');
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+
+
+const connection = require('./database/db');
+
+//9 - establecemos las rutas
+	app.get('/login',(req, res)=>{
+		res.render('login');
+	})
+
+	app.get('/register',(req, res)=>{
+		res.render('register');
+	})
