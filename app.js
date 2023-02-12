dd80a1714e28475bc8287d1652bf2ad98459910e
cristian-morris <christianyahirnavarrochan@gmail.com>
2023-02-12 01:30:24 -0600

    metodo de registro
    registro

--- app.js
+++ app.js
@@ -39,6 +39,30 @@ const connection = require('./database/db');
 		res.render('register');
 	})
 
+    app.post('/register', async (req, res)=>{
+        const user = req.body.user;
+        const name = req.body.name;
+        const rol = req.body.rol;
+        const pass = req.body.pass;
+        let passwordHash = await bcrypt.hash(pass, 8);
+        connection.query('INSERT INTO users SET ?',{user:user, name:name, rol:rol, pass:passwordHash}, async (error, results)=>{
+            if(error){
+                console.log(error);
+            }else{            
+                res.render('register', {
+                    alert: true,
+                    alertTitle: "Registration",
+                    alertMessage: "¡Successful Registration!",
+                    alertIcon:'success',
+                    showConfirmButton: false,
+                    timer: 1500,
+                    ruta: ''
+                });
+                    
+            }
+        });
+    })
+
     app.listen(3000, (req, res)=>{
         console.log('SERVER RUNNING IN http://localhost:3000');
     });
